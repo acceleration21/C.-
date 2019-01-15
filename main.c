529d66ae9947af0e11f0f2c22ac8774d76d2f824
wangjunze <171635094@qq.com>
2019-01-15 16:27:11 +0800

    退出和修补bug

--- main.c
+++ main.c
@@ -2,11 +2,10 @@
 
 int main()
 {
-    int qian=0;
+    int qian = 0;
     while (1)
     {
 
-        
         printf("1--存钱\n");
         printf("2--取钱\n");
         printf("3--余额\n");
@@ -30,28 +29,44 @@ int main()
         }
         if (code == 2)
         {
+            printf("输入要取的数额\n");
             int qu;
-            printf("请输入要取的余额\n");
             scanf("%d", &qu);
-            qian -= qu;
-            printf("取钱成功回车继续\n");
 
-            char x;
-            scanf("%c", &x);
-            scanf("%c", &x);
+            if (qu > qian)
+            {
+                printf("取钱实拍，余额不足\n");
+                char x;
+                scanf("%d", &x);
+                
+            }
+            else
+            {
+
+                qian -= qu;
+
+                printf("取钱成功回车继续\n");
+                char x;
+                scanf("%d", &x);
+                scanf("%d", &x);
+            }
         }
+
         if (code == 3)
         {
             printf("当前余额为:\n");
-            printf("%d\n",qian);
+            printf("%d\n", qian);
 
             printf("回车继续\n");
             char x;
             scanf("%c", &x);
             scanf("%c", &x);
         }
+
         if (code == 4)
         {
+            printf("回车退出\n");
+            break;
         }
     }
 
