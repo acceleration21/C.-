bf19bd20850e4ace2c4ee608ae88c3fc549e348c
wangjunze <171635094@qq.com>
2019-01-15 14:07:15 +0800

    取钱和余额

--- main.c
+++ main.c
@@ -2,15 +2,15 @@
 
 int main()
 {
-
+    int qian=0;
     while (1)
     {
 
-        int qian;
-        printf("1--存钱");
-        printf("2--取钱");
-        printf("3--余额");
-        printf("4--退出");
+        
+        printf("1--存钱\n");
+        printf("2--取钱\n");
+        printf("3--余额\n");
+        printf("4--退出\n");
 
         printf("请输入相应编码:\n");
         int code;
@@ -19,9 +19,10 @@ int main()
         if (code == 1)
         {
             int cun;
+            printf("输入要存钱的数额\n");
             scanf("%d", &cun);
             qian += cun;
-            printf("存錢成功，回車繼續\n");
+            printf("存钱成功，回车继续\n");
 
             char x;
             scanf("%c", &x);
@@ -29,9 +30,25 @@ int main()
         }
         if (code == 2)
         {
+            int qu;
+            printf("请输入要取的余额\n");
+            scanf("%d", &qu);
+            qian -= qu;
+            printf("取钱成功回车继续\n");
+
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 3)
         {
+            printf("当前余额为:\n");
+            printf("%d\n",qian);
+
+            printf("回车继续\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 4)
         {
